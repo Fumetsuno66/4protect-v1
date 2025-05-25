b1faba5b893c2165db66455205c12c0a348a611a
Fumetsuno66 <143916465+Fumetsuno66@users.noreply.github.com>
2025-05-25 12:11:50 +0200

    config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1089635366748835850', // Buyer
-        couleur: '#6495ED', // Couleur HEX
-        footer: 'mak', // Dans la commande help ou autre il y'a un texte en bas
+        couleur: '#FF0000', // Couleur HEX
+        footer: 'crownbot', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
